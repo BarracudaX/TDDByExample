12c33a0e66c39a4ee48d9c037e4b8c5edab7dca0
BarracudaX <dimitrijchannel@gmail.com>
2020-05-08 11:05:36 +0300

    Changes: 1)Added new Kata Example(Not implemented)

--- TDDByExample/src/test/java/berlin/BerlinTimeTest.java
+++ TDDByExample/src/test/java/berlin/BerlinTimeTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-//14
+//16
 public class BerlinTimeTest {
 
     BerlinClock berlinClock;
